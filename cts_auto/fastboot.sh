8b9d855ed20b233678059dc3a8cdf844c853f078
aoquan <qaoimu@163.com>
2016-04-04 20:20:09 +0800

    add some auto run script which will tell the android_x86's ipaddress to host linux when android_x86 start, and delete adb, so host linux need to install adb

--- cts_auto/fastboot.sh
+++ cts_auto/fastboot.sh
@@ -1,92 +1,89 @@
 #!/bin/bash
-mkdir ./android_disk
+ip_linux_host=192.168.2.36
+if [ ! -d "./android_disk" ]; then
+	mkdir ./android_disk
+fi
+
 umount ./android_disk
 mount /dev/sda2   ./android_disk
 
-mkdir  ./android_disk/android-2016-02-29/
-mkdir -p ./android_disk/android-2016-02-29/system
-mkdir -p ./android_disk/android-2016-02-29/data
+if [ ! -d "./android_disk/android-2016-02-29/" ]; then
+	mkdir  ./android_disk/android-2016-02-29/
+fi
+
+if [ ! -d "./android_disk/android-2016-02-29/system" ]; then
+	mkdir -p ./android_disk/android-2016-02-29/system
+fi
+
+if [ ! -d "./android_disk/android-2016-02-29/data" ]; then
+	mkdir -p ./android_disk/android-2016-02-29/data
+fi
 
 ## change the /system/etc/init.sh
 ## we need add statements to get the ip address of android_x86 and send it to linux
-## 
-ip=`getprot | grep ipaddress`
-ip=${ip##*\[}
-ip=${ip%]*}
-sendStr = "echo 'echo todosomething' | nc -q 0 $ip 5556"
-sed -i '$i\$sendStr'  ./android_disk/android-2016-02-29/system/etc/init.sh
+##
+
+## firstly, we check whether the second line from the bottom of init.sh is null(1) or "echo $ip | nc -q 0 $ip_linux_host 5556"(2)
+## condition (1) : we add  "echo $ip | nc -q 0 $ip_linux_host 5556"
+## else : replace this line with new statement
+line2bottom=`tail android_disk/android-2016-02-29/system/etc/init.sh -n 2 |head -n 1`
+if [ "$line2bottom" == "" ]; then
+	sed '$d' -i ./android_disk/android-2016-02-29/system/etc/init.sh
+	echo "ip=\`getprop | grep ipaddress\`
+	ip=\${ip##*\[}
+	ip=\${ip%]*}
+	echo \$ip | nc -q 0 $ip_linux_host 5556
+	return 0" >> ./android_disk/android-2016-02-29/system/etc/init.sh
+fi
 #######################################################
 
-echo $1
-sleep 2
-## ??? why sleep 2 ?
-
-if [ "$1" = "flashall" ]
-then
-cp  ./kernel        ./android_disk/android-2016-02-29/kernel
-cp  ./initrd.img    ./android_disk/android-2016-02-29/initrd.img
-cp  ./ramdisk.img   ./android_disk/android-2016-02-29/ramdisk.img
-
-mkdir   ./system_tmp
-sleep 2
-umount ./system_tmp
-mount ./system.img ./system_tmp
-rm -rf ./android_disk/android-2016-02-29/system/*
-cp   -Ra ./system_tmp/*   ./android_disk/android-2016-02-29/system
-umount ./system_tmp
+if [ "$1" = "flashall" ]; then
+	cp  ./kernel        ./android_disk/android-2016-02-29/kernel
+	cp  ./initrd.img    ./android_disk/android-2016-02-29/initrd.img
+	cp  ./ramdisk.img   ./android_disk/android-2016-02-29/ramdisk.img
+	mkdir   ./system_tmp
+	sleep 2
+	umount ./system_tmp
+	mount ./system.img ./system_tmp
+	rm -rf ./android_disk/android-2016-02-29/system/*
+	cp   -Ra ./system_tmp/*   ./android_disk/android-2016-02-29/system
+	umount ./system_tmp
 fi
 
 
-if [ "$1" = "kernel" ]
-then
-cp  ./kernel        ./android_disk/android-2016-02-29/kernel
+if [ "$1" = "kernel" ]; then
+	cp  ./kernel        ./android_disk/android-2016-02-29/kernel
 fi
 
-
-
-if [ "$1" = "initrd" ]
-then
-cp  ./initrd.img    ./android_disk/android-2016-02-29/initrd.img
+if [ "$1" = "initrd" ]; then
+	cp  ./initrd.img    ./android_disk/android-2016-02-29/initrd.img
 fi
 
-
-
-if [ "$1" = "ramdisk" ]
-then
-cp  ./ramdisk.img   ./android_disk/android-2016-02-29/ramdisk.img
+if [ "$1" = "ramdisk" ]; then
+	cp  ./ramdisk.img   ./android_disk/android-2016-02-29/ramdisk.img
 fi
 
-
-
-if [ "$1" = "system" ]
-then
-mkdir   ./system_tmp
-umount ./system_tmp
-mount ./system.img ./system_tmp
-rm -rf ./android_disk/android-2016-02-29/system/*
-cp   -Ra ./system_tmp/*   ./android_disk/android-2016-02-29/system
-umount ./system_tmp
+if [ "$1" = "system" ]; then
+	mkdir   ./system_tmp
+	umount ./system_tmp
+	mount ./system.img ./system_tmp
+	rm -rf ./android_disk/android-2016-02-29/system/*
+	cp -Ra ./system_tmp/*   ./android_disk/android-2016-02-29/system
+	umount ./system_tmp
 fi
 
-
-
-if [ "$1" = "reboot" ]
-then
-#efibootmgr -n {4}
-efibootmgr -n 6
-reboot
+if [ "$1" == "reboot" ]; then
+	efibootmgr -n 6
+	reboot
 fi
 
-
-
-if [ "$1" = "reboot-bootloader" ]
-then
-reboot
+if [ "$1" = "reboot-bootloader" ]; then
+	reboot
 fi
 
 
 ## install android_x86_iso
-if ["$1" = "install_iso"]
+if [ "$1" = "install_iso" ]
 	then
 	cd ~/android_auto
 	mkdir ./android_mnt
