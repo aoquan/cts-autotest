5fadf61e2d77573d1c1b7f6b63ca357d69171397
aoquan <qaoimu@163.com>
2016-04-18 18:00:00 +0800

    auto test in virtual mechine

--- autoTest.sh
+++ autoTest.sh
@@ -23,13 +23,41 @@ ip_linux_host=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '
 ## according to where it's virtual mechine(qemu) or real mechine, we should change the network model
 if [ "$1" == "v" ]; then
 	if [ "$2" == "iso" ]; then
-		/home/aquan/work/qemu-2.5.0/x86_64-softmmu/qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no android_x86.iso 
+		#/home/aquan/work/qemu-2.5.0/x86_64-softmmu/qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no android_x86.iso 
+		##qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no android_x86.iso 
+		## install iso in to raw
+		mkdir android_raw;
+		mkdir android_mnt;
+		mkdir system_tmp;
+		mount -o loop,offset=32256 ~/git/cts-autotest/rawiso/android_x86.raw android_raw;
+		mount ~/git/cts-autotest/rawiso/android_x86_64_4.4.iso  android_mnt;
+		mount android_mnt/system.img system_tmp;
+
+		rm android_raw/android-2016-02-29/* -r;
+		mkdir android_raw/android-2016-02-29/system;
+		cp   -Ra ./system_tmp/*   ./android_raw/android-2016-02-29/system;
+		cp  ./android_mnt/kernel         android_raw/;
+		cp  ./android_mnt/initrd.img     android_raw/;
+		cp  ./android_mnt/ramdisk.img    android_raw/;
+		cp  ./android_mnt/system.img     android_raw/;
+
+		umount system_tmp;
+		umount android_mnt;
+		umount android_raw;
+
+		rm -r system_tmp;
+		rm -r android_mnt;
+		rm -r android_raw;
+
+		qemu-system-x86_64 -m 4G --enable-kvm /home/aquan/work/cts/android_x86.raw
 	elif [ "$2" == "raw" ];then
 		#######################################################
 		## replace file specified by user
 		./virFastboot.sh $3
 
-		/home/aquan/work/qemu-2.5.0/x86_64-softmmu/qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no /home/aquan/work/cts/android_x86.raw &
+
+		#/home/aquan/work/qemu-2.5.0/x86_64-softmmu/qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no /home/aquan/work/cts/android_x86.raw &
+		qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no /home/aquan/work/cts/android_x86.raw &
 		{
 			ip_android_v=`nc -lp 5556`
 			adb connect $ip_android_v
