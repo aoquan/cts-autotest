33ed19c89f1413259053fae936f244668f54c0e8
aquan <qaoimu@163.com>
2016-04-20 21:02:19 +0800

    make the input parameterized

--- autoTest.sh
+++ autoTest.sh
@@ -9,73 +9,86 @@
 ## change the java version command
 ## update-alternatives --config java
 
+#################################################################
 ## $1 : virtual mechine or real mechine (v/r)
-## $2 : run android_x86.iso or raw in qemu (iso/raw)
-## $3 : ip_linux_client
+## $2 : run android_x86.iso or raw in qemu (iso/raw), for automatic installation iso have not been implemented, you can not input raw  
+## $3 : if $1=v, $3 stand for location of raw or iso
+##      if $1=r, $3 stand for location of android_x86 system (which sda?)
+## $4 : cts command
 
 ## example:
-## virtual mechine: sudo ./autoTest.sh v raw
-## real mechine: sudo ./autoTest.sh r 192.168.2.16 
+## virtual mechine: sudo ./autoTest.sh v raw ../rawiso/android_x86.raw "--plan CTS --disable-reboot"
+## real mechine: sudo ./autoTest.sh r 192.168.2.16 /dev/sda4 "--plan CTS --disable-reboot"
+#################################################################
 
 
 ip_linux_host=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+ctsCmd="$4"
 
 ## according to where it's virtual mechine(qemu) or real mechine, we should change the network model
 if [ "$1" == "v" ]; then
-	if [ "$2" == "iso" ]; then
-		#/home/aquan/work/qemu-2.5.0/x86_64-softmmu/qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no android_x86.iso 
-		##qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no android_x86.iso 
-		## install iso in to raw
-		mkdir android_raw;
-		mkdir android_mnt;
-		mkdir system_tmp;
-		mount -o loop,offset=32256 ~/git/cts-autotest/rawiso/android_x86.raw android_raw;
-		mount ~/git/cts-autotest/rawiso/android_x86_64_4.4.iso  android_mnt;
-		mount android_mnt/system.img system_tmp;
+	if [ "$2" == "raw" ];then
+		rawIsoLocation=$3
 
-		rm android_raw/android-2016-02-29/* -r;
-		mkdir android_raw/android-2016-02-29/system;
-		cp   -Ra ./system_tmp/*   ./android_raw/android-2016-02-29/system;
-		cp  ./android_mnt/kernel         android_raw/;
-		cp  ./android_mnt/initrd.img     android_raw/;
-		cp  ./android_mnt/ramdisk.img    android_raw/;
-		cp  ./android_mnt/system.img     android_raw/;
+		if [ ! -d "android_disk" ]; then
+			mkdir  android_disk
+		fi
+		mount -o loop,offset=32256 $rawIsoLocation android_disk;
+		sed '$d' -i ./android_disk/android*/system/etc/init.sh
+		sed '$d' -i ./android_disk/android*/system/etc/init.sh
+		echo "echo \$ip | nc -q 0 $ip_linux_host 5556
+			return
+			" >> ./android_disk/android*/system/etc/init.sh
+		umount android_disk;
 
-		umount system_tmp;
-		umount android_mnt;
-		umount android_raw;
-
-		rm -r system_tmp;
-		rm -r android_mnt;
-		rm -r android_raw;
-
-		qemu-system-x86_64 -m 4G --enable-kvm /home/aquan/work/cts/android_x86.raw
-	elif [ "$2" == "raw" ];then
-		#######################################################
-		## replace file specified by user
-		./virFastboot.sh $3
-
-
-		#/home/aquan/work/qemu-2.5.0/x86_64-softmmu/qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no /home/aquan/work/cts/android_x86.raw &
-		qemu-system-x86_64 -m 4G --enable-kvm -net nic,vlan=0 -net tap,vlan=0,ifname=tap0,script=no /home/aquan/work/cts/android_x86.raw &
+		qemu-system-x86_64 -m 2G --enable-kvm -net nic -net user,hostfwd=tcp::5557-:5555 $rawIsoLocation &
 		{
 			ip_android_v=`nc -lp 5556`
-			adb connect $ip_android_v
-			../android-cts/tools/cts-tradefed run cts --plan CTS --disable-reboot  
-			adb disconnect $ip_android_v	
+			## waiting for a message from android-x86, this ip address is useful in real mechine test, but in virtural mechine ,we adopt nat address mapping ,
+			## so it's just a symbol that android-x86 is running 
+			adb connect localhost:5557
+			#../android-cts/tools/cts-tradefed run cts --plan CTS --disable-reboot
+			../android-cts/tools/cts-tradefed run cts $ctsCmd
+			adb disconnect localhost:5557
 		}
 	fi
 elif [ "$1" == "r" ];then
 	## real mechine
 	ip_linux_client=$2
-	autoFrame/android_tool.sh $ip_linux_client fastboot.sh $3
+	androidLocation=$3
+	## loction(sda) of android system 
 
+	ssh root@${ip_linux_client}
+	if [ ! -d "android_disk" ]; then
+			mkdir  android_disk
+	fi
+	#mount /dev/sda4 android_disk;
+	mount androidLocation android_disk;
 
+	line2bottom=`tail android_disk/android*/system/etc/init.sh -n 2 |head -n 1`
+
+	sed '$d' -i ./android_disk/android*/system/etc/init.sh
+	sed '$d' -i ./android_disk/android*/system/etc/init.sh
+
+	if [ "$line2bottom" == "" ]; then
+		echo "ip=\`getprop | grep ipaddress\`
+		ip=\${ip##*\[}
+		ip=\${ip%]*}
+		echo \$ip | nc -q 0 $ip_linux_host 5556
+		return 0" >> ./android_disk/android*/system/etc/init.sh
+	else
+		echo "		echo \$ip | nc -q 0 $ip_linux_host 5556
+		return 0" >> ./android_disk/android*/system/etc/init.sh
+	fi
+	umount android_disk;
+	efibootmgr -n 6
+	reboot
+
+	## return to linux_host command
 	ip_android_r=`nc -lp 5556`
 	adb connect $ip_android_r
 	../android-cts/tools/cts-tradefed run cts --plan CTS --disable-reboot  
 	adb disconnect $ip_android_r
-
 fi
 
 
